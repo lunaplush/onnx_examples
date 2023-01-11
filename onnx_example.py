07f8be2997539b8033e88676a78a04d965b9d509
lunaplus <lunaplus09@yandex.ru>
2023-01-11 13:08:56 +0300

    crypto_bert convert to onnx example

--- onnx_example.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pickle
-import os
-
-
-with open("logit_tfidf_btc_sentiment.pkl", "rb") as f:
-     model =  pickle.load(f)
--- super_resolution.py
+++
@@ -1,6 +0,0 @@
-import io
-import numpy as np
-
-from torch import nn
-import torch.utils.model_zoo as model_zoo
-import torch.onnx
